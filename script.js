73a31eae5a22d591b0aae86548e2249aad082d23
goldenegbua3 <96621335+goldenegbua3@users.noreply.github.com>
2022-06-24 20:14:07 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Simple arithmetic calculator
+//input operator
+let operator = prompt('Enter operator ( type +, -, *, or /): ');
+
+//input first number
+let number1 = parseFloat(prompt('Enter first number: '));
+
+// input second number
+let number2 = parseFloat(prompt('Enter second number: '));
+
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+console.log(​'${number1} ${operator} ${number2} = ${result}');
